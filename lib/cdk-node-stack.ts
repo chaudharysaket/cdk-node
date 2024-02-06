82265d7d7eb49502478a83ec7bb34ce160e6ca55
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-02-07 01:05:43 +0530

    initial commit

--- lib/cdk-node-stack.ts
+++ lib/cdk-node-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-export class CdkNodeStack extends cdk.Stack {
+export class DockerLambdaAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the Lambda function
+    const lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,    // Runtime
+      code: lambda.Code.fromAsset('./image'), // Code loaded from the "path/to/your/code" directory
+      handler: 'index.handler'                // File is "app", function is "handler"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkNodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // other resources (like API Gateway) can be defined here
   }
 }
