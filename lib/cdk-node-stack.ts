1150dbea8ad3ecda7b7ce50da67dfd7992705b96
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-02-07 09:00:19 +0530

    add env variables

--- lib/cdk-node-stack.ts
+++ lib/cdk-node-stack.ts
@@ -7,10 +7,24 @@ export class DockerLambdaAwsStack extends cdk.Stack {
     super(scope, id, props);
 
     // Define the Lambda function
-    const lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
-      runtime: lambda.Runtime.NODEJS_14_X,    // Runtime
-      code: lambda.Code.fromAsset('./image'), // Code loaded from the "path/to/your/code" directory
-      handler: 'index.handler'                // File is "app", function is "handler"
+    const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
+      code: lambda.DockerImageCode.fromImageAsset( "./image", { 
+        cmd: ['newrelic_lambda_wrapper.handler'] // assign cmd override for image config 
+      } ), 
+      memorySize: 1024, // MB
+      timeout: cdk.Duration.seconds(10),
+      architecture: lambda.Architecture.ARM_64, // needed for M1 mac
+      tracing: lambda.Tracing.ACTIVE, // add xray action to policy
+      environment: {
+        NEW_RELIC_TRUSTED_ACCOUNT_KEY : "<NEW_RELIC_PARENT_ID>",
+        NEW_RELIC_ACCOUNT_ID : "<NEW_RELIC_ACCOUNT_ID>",
+        NEW_RELIC_EXTENSION_SEND_FUNCTION_LOGS : "true",
+        NEW_RELIC_LAMBDA_EXTENSION_ENABLED : "true",
+        NEW_RELIC_LAMBDA_HANDLER : "main.handler",
+        NEW_RELIC_LICENSE_KEY : "<NEW_RELIC_LICENSE_KEY>",
+        NEW_RELIC_LOG_ENDPOINT : "https://log-api.newrelic.com/log/v1" ,
+        NEW_RELIC_TELEMETRY_ENDPOINT : "https://cloud-collector.newrelic.com/aws/lambda/v1",
+      }
     });
 
     // other resources (like API Gateway) can be defined here
